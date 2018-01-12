81b74d3171bb8b24bbc6dab8aab60d460251b1b2
Min Kim <mk4555@nyu.edu>
2018-01-12 17:24:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,6 +25,13 @@ class App < Sinatra::Base
     num2 = params[:number2].to_i
     case params[:operation]
     when "add"
-
+      "#{num1 + num2}"
+    when "multiply"
+      "#{num1 * num2}"
+    when "divide"
+      "#{num1 / num2}"
+    when "subtract"
+      "#{num1 - num2}"
+    end
   end
 end
